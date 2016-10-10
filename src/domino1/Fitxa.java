7164a7eb70410a511dd73fad0bd3ee75fdd3505c
Roger G. Coscojuela <rogilico@gmail.com>
2016-10-10 21:28:22 +0200

    Canvis

--- src/domino1/Fitxa.java
+++ src/domino1/Fitxa.java
@@ -4,26 +4,29 @@
  * and open the template in the editor.
  */
 package domino1;
+
 public class Fitxa {
-    int Valor1;
-    int Valor2;
-    private int[] Fitxa = new int [2];
-   
-    public Fitxa (int Valor1, int Valor2){
-        this.Valor1=Valor1;
-        this.Valor2=Valor2;
+
+    private int[] Valor;
+
+    public Fitxa(int[] Valor) {
+        this.Valor = Valor;
     }
-    
-    public int[] getFitxa() {
-        return this.Fitxa;
-        
+
+    public int[] getValor() {
+        return Valor;
     }
-    public int[] Invertir (int[] Fitxa){ /*Invertir el orden de los valores de 
-        fitxa*/
-        int aux = Fitxa [0];
-        Fitxa [0]=Fitxa [1];
-        Fitxa [1]= aux;
-        return Fitxa;
+
+    public void Invertir() { /*Invertir el orden de los valores de 
+         fitxa*/
+
+        int aux = Valor[0];
+        Valor[0] = Valor[1];
+        Valor[1] = aux;
     }
-    
+
+    @Override
+    public String toString() {
+        return "Fitxa{" + "Valor=" + Valor[0] + "," + Valor[1]+"}\n";
+    }
 }
--- src/domino1/Partida.java
+++ src/domino1/Partida.java
@@ -4,18 +4,34 @@
  * and open the template in the editor.
  */
 package domino1;
-
+import java.util.ArrayList;
 /**
  *
  * @author IES
  */
 public class Partida {
-      private int NombreJugadors = 4;
-      private int NombreFitxes = 28;
-      
-      public Partida (int NombreJugadors) {
-          
-      }
-      
-    
+
+    private int NombreJugadors = 4;
+    private int NombreFitxes = 28;
+    private ArrayList<Fitxa> fitxes;
+
+    public Partida(int NombreJugadors) {
+        this.NombreJugadors = NombreJugadors;
+        fitxes = new ArrayList<>();
+    }
+
+    public void crearFitxes() {
+        for (int i = 0; i <= 6; i++) {
+            for (int j = i; j <= 6; j++) {
+                int [] aux = {i,j};
+            fitxes.add(new Fitxa(aux));
+            }
+        }
+
+    }
+
+    public ArrayList<Fitxa> getFitxes() {
+        return fitxes;
+    }
+
 }
